cac18412245ce585635f435f7a4efbf7c32ebfe5
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2021-07-03 16:44:15 +0200

    Aggiunto render-mixin

--- client/js/order-dialog.mjs
+++ client/js/order-dialog.mjs
@@ -1,8 +1,9 @@
 import dialogMixin from './dialog-mixin.mjs'
+import renderMixin from './render-mixin.mjs'
 
 export default {
   name: 'OrderDialog',
-  mixins: [dialogMixin],
+  mixins: [dialogMixin, renderMixin],
   props: {
     params: {
       type: Object,
@@ -19,12 +20,22 @@ export default {
         cust_code: null,
         agent_code: null,
         order_description: null
-      }
+      },
+      customers: []
     }
   },
   computed: {
     isNew () {
       return !this.order?.ord_num
+    },
+    customerOptions () {
+      /* avrei potuto far restituire al server già la lista così come mi serve, ma disaccoppiamo le cose... */
+      return this.customers.map(cust => {
+        return {
+          key: cust.code,
+          label: cust.name
+        }
+      })
     }
   },
   methods: {
@@ -32,7 +43,16 @@ export default {
       return this.isNew ? 'Nuovo ordine' : 'Modifica ordine'
     },
     createBodyHtml(h) {
-      return this.order?.ord_num
+      // return this.order?.ord_num
+      return [
+        this.createInputHtml(h, { label: 'Numero', disabled: true, placeholder: '-' }, this.order, 'ord_num'),
+        this.createInputHtml(h, { label: 'Data', type: 'date' }, this.order, 'ord_date'),
+        this.createTextareaHtml(h, { label: 'Descrizione', placeholder: 'Contenuto ordine' }, this.order, 'order_description'),
+        this.createInputHtml(h, { label: 'Anticipo', type: 'number', placeholder: '100.00' }, this.order, 'advance_amount'),
+        this.createInputHtml(h, { label: 'Totale', type: 'number', placeholder: '1000.00' }, this.order, 'ord_amount'),
+        this.createSelectHtml(h, { label: 'Cliente', options: this.customerOptions }, this.order, 'cust_code'),
+        h('pre', JSON.stringify(this.order))
+      ]
     },
     createFooterHtml(h) {
       return [
@@ -52,6 +72,11 @@ export default {
     }
   },
   mounted () {
+    this.$store.dispatch('loadCustomersResume').then(customers => {
+      this.customers.splice(0, 0, ...customers)
+    }).catch(() => {
+      this.reject()
+    })
     Object.entries(this.params).forEach((key_value) => {
       this.$set(this.order, key_value[0], key_value[1])
     })
---
+++ client/js/render-mixin.mjs
@@ -0,0 +1,73 @@
+export default {
+  methods: {
+    createInputHtml(h, params, model, key) {
+      const self = this
+      params = Object.assign({ type: 'text', disabled: false, placeholder: null }, params)
+      return h('div', {
+        class: 'input-box-2'
+      }, [
+        h('label', params.label),
+        h('input', {
+          domProps: {
+            type: params.type,
+            disabled: params.disabled,
+            placeholder: params.placeholder,
+            value: model[key]
+          },
+          on: {
+            input: function (event) {
+              self.$set(model, key, event.target.value)
+            }
+          }
+        })
+      ])
+    },
+    createSelectHtml(h, params, model, key) {
+      const self = this
+      params = Object.assign({ disabled: false, placeholder: null, options: [] }, params)
+      return h('div', {
+        class: 'input-box-2'
+      }, [
+        h('label', params.label),
+        h('select', {
+          domProps: {
+            disabled: params.disabled,
+            placeholder: params.placeholder,
+            value: model[key]
+          },
+          on: {
+            change: function (event) {
+              self.$set(model, key, event.target.value)
+            }
+          }
+        }, params.options.map(opt => h('option', {
+          domProps: {
+            value: opt.key,
+            selected: opt.key === model[key]
+          }
+        }, opt.label)))
+      ])
+    },
+    createTextareaHtml(h, params, model, key) {
+      const self = this
+      params = Object.assign({ disabled: false, placeholder: null }, params)
+      return h('div', {
+        class: 'input-box-2'
+      }, [
+        h('label', params.label),
+        h('textarea', {
+          domProps: {
+            disabled: params.disabled,
+            placeholder: params.placeholder,
+            value: model[key]
+          },
+          on: {
+            input: function (event) {
+              self.$set(model, key, event.target.value)
+            }
+          }
+        })
+      ])
+    },
+  }
+}
